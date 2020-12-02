821be667d25fffaae7ea3a54e1f3649bcb7b1f23
NotoriousRebel <matt12299@yahoo.com>
2020-12-02 16:33:30 -0500

    Part 2 done.

--- Day2/sol.nim
+++ Day2/sol.nim
@@ -29,8 +29,26 @@ proc part1(): Future[int] {.async.}=
             inc isValidCounter
     result = isValidCounter
 
+proc part2(): Future[int] {.async.} = 
+     # var letterToCount = initTable[char, int]()
+    var isValidCounter = 0
+    var file = openAsync("input.txt", fmRead)
+    let data = await file.readAll()
+    file.close()
+    let lines = splitLines(data)
+    for line in lines:
+        # Example: line: 5-6 g: kgggjg
+        let parts = line.split(" ")
+        let firstNum = parseInt(parts[0].split("-")[0])
+        let secondNum = parseInt(parts[0].split("-")[1])
+        let targetChar = parts[1][0]
+        if  (parts[2][firstNum-1] == targetChar and parts[2][secondNum-1] != targetChar) or (parts[2][firstNum-1] != targetChar and parts[2][secondNum-1] == targetChar):
+            echo "Located valid password: ", $line
+            inc isValidCounter
+    result = isValidCounter
+
 proc main() {.async.} = 
     echo "First part sol: ", await part1()
-
+    echo "Second part sol: ", await part2()
 
 waitFor main()
